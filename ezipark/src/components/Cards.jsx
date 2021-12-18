f022d057d98b982e222daa9230a244f14c667f91
zakari64 <74094535+zakari64@users.noreply.github.com>
2021-12-18 15:24:32 +0100

    adjustments
    still working on other menu items divs

--- ezipark/src/components/Cards.jsx
+++ ezipark/src/components/Cards.jsx
@@ -1,60 +1,56 @@
 import React from "react";
 import cards from "../styles/cards.css";
-import wallet from "../assets/images/wallet.png";
-import watch from "../assets/images/001-clock.png";
-import like from "../assets/images/001-like.png";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-
+import park from '../assets/images/parking-area.png'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Cards() {
   AOS.init({
-    duration : 1000
-  })
+    duration: 1000,
+  });
 
-  return ( 
-   
+  return (
     <section className="cards">
+      <div className="title" data-aos="fade-down-right">
+        <h1>What we provide you with </h1>
+      </div>
       <div className="cards-content">
-
-        <div className="card" data-aos='fade-down-right'>
+        <div className="card" data-aos="fade-down-right">
           <div className="container">
-             <div className="img-container">
-              <img src={wallet} alt="wallet" />
-             </div>
-             <div className="txt-container">
-             <h2>Save Money</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, aperiam!</p>
-             </div>
+            <div className="img-container">
+              <img src={park} alt="wallet" />
+            </div>
+            <div className="txt-container">
+              <h2>Save Money</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            </div>
           </div>
         </div>
 
-        <div className="card" data-aos='fade-down-right'>
+        <div className="card" data-aos="fade-down-right">
           <div className="container">
-             <div className="img-container">
-              <img src={like} alt="like" />
-             </div>
-             <div className="txt-container">
-             <h2>Stress Less</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, aperiam!</p>
-             </div>
+            <div className="img-container">
+              <img src={park} alt="like" />
+            </div>
+            <div className="txt-container">
+              <h2>Stress Less</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
           </div>
         </div>
-        
-       <div className="card" data-aos='fade-down-right'>
+
+        <div className="card" data-aos="fade-down-right">
           <div className="container">
-             <div className="img-container">
-              <img src={watch} alt="watch" />
-             </div>
-             <div className="txt-container">
-             <h2>Save Time</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, aperiam!</p>
-             </div>
+            <div className="img-container">
+              <img src={park} alt="watch" />
+            </div>
+            <div className="txt-container">
+              <h2>Save Time</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            </div>
           </div>
         </div>
       </div>
-
     </section>
   );
 }
