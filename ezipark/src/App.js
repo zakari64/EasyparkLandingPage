53c6971309f9a470b77fc217e80edda1289ecfd5
zakari64 <74094535+zakari64@users.noreply.github.com>
2021-12-31 17:39:14 +0100

    added nav links

--- ezipark/src/App.js
+++ ezipark/src/App.js
@@ -1,14 +1,27 @@
 import react from "react";
 import OwlCarousel from "react-owl-carousel2";
 import HomePage from "./components/HomePage";
+import Cards from "./components/Cards";
+import Contact from "./components/Contact";
+import { BrowserRouter as Router , Switch , Route } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Testimonial from "./components/Testimonial";
+import Container from "./components/Container";
+
+
 function App() {
   return (
-    
-
-      <div className="App">
-        <HomePage />
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/"  component={HomePage} />
+          <Route path="/container" component={Container} />
+          <Route path="/cards" component={Cards} />
+          <Route path="/testimonial" component={Testimonial} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
       </div>
+    </Router>
       
 
     
