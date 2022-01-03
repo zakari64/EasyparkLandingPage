73d46db307e79a910cc49f58ac9e363d6e4b375f
zakari64 <zakariaben415@gmail.com>
2022-01-03 12:06:56 +0100

    changed backgrounds

--- ezipark/src/components/Container.jsx
+++ ezipark/src/components/Container.jsx
@@ -1,30 +1,36 @@
 import React from "react";
 import container from "../styles/container.css";
-import AOS from 'aos';
-import car from '../assets/images/parking.png'
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import car from "../assets/images/parking.png";
+import "aos/dist/aos.css";
 
 function Container() {
   AOS.init({
-    duration : 1000
-  })
+    duration: 1000,
+  });
   return (
     <main id="sect1">
       <div className="lefty">
         <div className="title">
-          <h1 data-aos="zoom-in-down">We make parking a lot easier with Parkeasy!</h1>
+          <h1
+            data-aos="fade-right"
+            data-aos-offset="300"
+            data-aos-easing="ease-in-sine"
+          >
+            We make parking a lot easier with Parkeasy!
+          </h1>
         </div>
         <div className="txt-content">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo
           consectetur quidem tenetur animi libero nemo, ea expedita sequi quasi
           aut!
         </div>
-        <button className="button-64" role="button">
+        <button data-aos="zoom-in-down" className="button-64" role="button">
           <span className="text">Get Started !</span>
         </button>
       </div>
       <div className="righty">
-        <div className="img-are">
+        <div data-aos="zoom-in-down" className="img-are">
           <img src={car} alt="" />
         </div>
       </div>
